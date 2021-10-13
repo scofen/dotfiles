03adb7f0acb3d48725adf9507112c9169f8f9946
dkelll <dkelll@users.noreply.github.com>
2021-10-13 14:30:19 -0500

    Check for grc.bashrc file existing before sourcing it

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,5 @@
 # GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] ))
+if (( $+commands[grc] )) && (( $+commands[brew] )) && [[ -a `brew --prefix`/etc/grc.bashrc ]] 
 then
   source `brew --prefix`/etc/grc.bashrc
 fi
