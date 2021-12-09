a9bf153a0fc326f8960d8185b941e2639fddb5ae
dkelll <dkelll@users.noreply.github.com>
2021-12-09 10:49:52 -0600

    Test the waters for pruning remote branches that have died

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -22,3 +22,5 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+alias gprune='git remote prune origin'
+#alias gprune="git fetch -p && for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}'); do git branch -D $branch; done"
