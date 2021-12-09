da36f99229f55ac4e6f61966d794e60cd45cf349
dkelll <dkelll@users.noreply.github.com>
2021-12-09 10:50:18 -0600

    Add update capability for sfdx

--- node/install.sh
+++ node/install.sh
@@ -2,7 +2,9 @@
 if test ! $(which sfdx)
 then
   npm install -g sfdx-cli
+else 
+  npm update --global sfdx-cli
 fi
 
 # Setup sfdx autocomplete
-printf "eval $(sfdx autocomplete:script zsh)" >> ~/.zshrc; source ~/.zshrc
+#printf "eval $(sfdx autocomplete:script zsh)" >> ~/.zshrc; source ~/.zshrc
